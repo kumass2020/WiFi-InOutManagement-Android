994f6466aa8fa75d5ec8592a8b57a9482b49e4d8
hoho <kumass2021@gmail.com>
2021-05-02 19:57:40 +0900

    add: local network ip
    for test

--- InOutManagement/app/src/main/java/com/example/inoutmanagement/RetrofitConnection.java
+++ InOutManagement/app/src/main/java/com/example/inoutmanagement/RetrofitConnection.java
@@ -12,8 +12,11 @@ public class RetrofitConnection {
 
     OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder().addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
 
+    // Test URL(Loopback or local network)
+    String URL = "http://172.30.1.23:40012";
+
     // Server URL(내부)
-    String URL = "http://192.9.45.226:40006/";
+//    String URL = "http://192.9.45.226:40006/";
 
 //    // Server URL(외부)
 //    String URL = "http://210.102.181.156:40006/";
