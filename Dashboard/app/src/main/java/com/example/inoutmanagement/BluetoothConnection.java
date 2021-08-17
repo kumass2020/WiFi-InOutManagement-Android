d96379f7c2b86f5a434b8b94b2fdd8ab701ca37d
hoho <kumass2021@gmail.com>
2021-08-17 09:39:58 +0900

    add: dashboard ver.

---
+++ Dashboard/app/src/main/java/com/example/inoutmanagement/BluetoothConnection.java
@@ -0,0 +1,13 @@
+package com.example.inoutmanagement;
+
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+public class BluetoothConnection {
+    // Use this check to determine whether BLE is supported on the device. Then
+    // you can selectively disable BLE-related features.
+//    if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+//            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+//            finish();
+//    }
+}
